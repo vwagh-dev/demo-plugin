c45d2f9481a808cb97a96f7e984066a958654ee0
Vishal Wagh <vwagh@cloudbees.com>
2024-07-12 08:56:46 +0530

    Added form validation i.e. level2

--- src/main/java/io/jenkins/plugins/sample/global_configuration/OnboardingPluginGlobalConfiguration.java
+++ src/main/java/io/jenkins/plugins/sample/global_configuration/OnboardingPluginGlobalConfiguration.java
@@ -1,11 +1,23 @@
 package io.jenkins.plugins.sample.global_configuration;
 
 import hudson.Extension;
+import hudson.Util;
+import hudson.util.FormValidation;
+import hudson.util.QueryParameterMap;
 import jenkins.model.GlobalConfiguration;
 import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.QueryParameter;
+
 
 @Extension
 @Symbol("OnboardingPlugin")
 public class OnboardingPluginGlobalConfiguration extends GlobalConfiguration {
 
+    public FormValidation doCheckName(@QueryParameter String name) {
+        String regex = "^[a-zA-Z ]+$";
+        if (!name.matches(regex)) {
+            return FormValidation.warning("Name must contains characters & spaces");
+        }
+        return FormValidation.ok();
+    }
 }
