4d7b5408215476aa17ea0e774b1b96662df4bbba
Vishal Wagh <vwagh@cloudbees.com>
2024-07-12 22:17:03 +0530

    Added getter/setter for the form fields.

--- src/main/java/io/jenkins/plugins/sample/global_configuration/OnboardingPluginGlobalConfiguration.java
+++ src/main/java/io/jenkins/plugins/sample/global_configuration/OnboardingPluginGlobalConfiguration.java
@@ -7,5 +7,22 @@ import org.jenkinsci.Symbol;
 @Extension
 @Symbol("OnboardingPlugin")
 public class OnboardingPluginGlobalConfiguration extends GlobalConfiguration {
+    private String name;
+    private String description;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
